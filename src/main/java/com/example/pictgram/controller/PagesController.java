27e2dc2b692a669e2f38355c5abc46e202330690
yongwoo <ywl0806@gmail.com>
2022-02-03 22:09:34 +0900

    2

--- src/main/java/com/example/pictgram/controller/PagesController.java
+++ src/main/java/com/example/pictgram/controller/PagesController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PagesController {
 
-	@GetMapping("/")
-	public String index() {
-		return "pages/index";
-	}
+    @GetMapping("/")
+    public String index() {
+        return "pages/index";
+    }
 }
