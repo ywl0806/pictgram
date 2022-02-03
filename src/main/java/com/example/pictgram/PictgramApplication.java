a821cd7213d7f2cf63acc2c179ba1af7661a2b28
yongwoo <ywl0806@gmail.com>
2022-02-03 10:57:02 +0900

    0203

---
+++ src/main/java/com/example/pictgram/PictgramApplication.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pictgram/PictgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pictgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
